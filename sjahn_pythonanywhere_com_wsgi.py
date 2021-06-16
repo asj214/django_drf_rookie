ff02132a40ca30f2bb40efc8682fba49c090f41b
sjahn <asj214@naver.com>
2021-06-16 16:51:01 +0900

    update

--- sjahn_pythonanywhere_com_wsgi.py
+++ sjahn_pythonanywhere_com_wsgi.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 
 path = '/home/sjahn/django_drf_rookie'  # PythonAnywhere 계정으로 바꾸세요.
@@ -8,6 +10,4 @@ if path not in sys.path:
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'configs.settings'
 
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
 application = StaticFilesHandler(get_wsgi_application())
