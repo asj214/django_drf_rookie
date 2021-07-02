e7f6080bce57b5543c51c863964f06d3d24cfd59
sjahn <asj214@naver.com>
2021-07-02 18:17:20 +0900

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 python manage.py migrate
 
 python manage.py loaddata users/fixtures/users.yaml
-
-python manage.py dumpdata users.User --format=yaml > users/fixtures/users.yaml
-python manage.py dumpdata pages.Page --format=yaml > pages/fixtures/pages.yaml
-python manage.py dumpdata menus.Menu --format=yaml > menus/fixtures/menus.yaml
+python manage.py loaddata pages/fixtures/pages.yaml
+python manage.py loaddata menus/fixtures/menus.yaml
