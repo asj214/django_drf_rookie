7578628c77d2a10aef659fe402c54a1841c95ad6
sjahn <asj214@naver.com>
2021-10-22 21:17:47 +0900

    update

--- init.sh
+++
@@ -1,5 +0,0 @@
-python manage.py migrate
-
-python manage.py loaddata users/fixtures/users.yaml
-python manage.py loaddata pages/fixtures/pages.yaml
-python manage.py loaddata menus/fixtures/menus.yaml
